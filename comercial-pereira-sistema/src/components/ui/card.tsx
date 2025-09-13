efb7ffc58c9979169929c7def3b785e5c9a6c2c6
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2025-09-13 18:24:57 -0300

    Adição components do radix

--- comercial-pereira-sistema/src/components/ui/card.tsx
+++ comercial-pereira-sistema/src/components/ui/card.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
-import { cn } from "@/lib/utils"
+import {cn} from "@/lib/utils/utils";
+
 
 const Card = React.forwardRef<
     HTMLDivElement,
@@ -7,7 +8,8 @@ const Card = React.forwardRef<
 >(({ className, ...props }, ref) => (
     <div
         ref={ref}
-        className={cn(
+        className={
+        cn(
             "rounded-lg border bg-card text-card-foreground shadow-sm",
             className
         )}
